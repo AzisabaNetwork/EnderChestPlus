3d52552dedb09555dd4731ae03189ac9ad33581d
albardoo2 <akshs@outlook.jp>
2026-08-02 22:35:30 +0900

    chore: remove redundant archive file name configuration in shadowJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,6 @@ tasks {
         dependsOn(shadowJar)
     }
     shadowJar {
-        archiveFileName.set("EnderChestPlus.jar")
         archiveClassifier.set("")
         relocate("co.aikar.taskchain", "jp.azisaba.lgw.ecplus.depends.taskchain")
         relocate("com.zaxxer.hikari", "jp.azisaba.lgw.ecplus.depends.hikari")
