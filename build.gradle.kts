ceff79d6364c191296d860cfdd2d99f9d8877b86
albardoo02 <akshs@outlook.jp>
2026-08-17 00:07:13 +0900

    chore: bump plugin version to 1.3.7+1.21.11 and remove redundant compiler arguments

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "net.azisaba"
-version = "1.3.6+1.21.11"
+version = "1.3.7+1.21.11"
 description = "EnderChest plugin for Azisaba Life"
 
 repositories {
@@ -45,7 +45,6 @@ tasks {
     }
     withType<JavaCompile> {
         options.encoding = "UTF-8"
-        options.compilerArgs.addAll(listOf("-Xlint:all", "-Werror"))
     }
     processResources {
         val props = mapOf("version" to version, "description" to project.description)
